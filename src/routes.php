dfbf239154112704e42c32ad13dcc29675361316
Stef van Esch <stef@marshmallow.dev>
2021-10-23 11:55:35 +0200

    Removed unused route

--- src/routes.php
+++ src/routes.php
@@ -3,5 +3,5 @@
 Route::group(['namespace' => 'Marshmallow\Product\Http\Controllers'], function () {
     // Route::post('/', 'ProductController@index');
     Route::get('/feeds/products/google', 'ProductFeedController@google');
-    Route::get('/shop/{product:slug}', 'ProductController@show')->name('product.detail');
+    // Route::get('/shop/{product:slug}', 'ProductController@show')->name('product.detail');
 });
