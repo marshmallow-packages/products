8e85ec3f5785837e269600bcffb1db8ce21357f3
Stef van Esch <stef@marshmallow.dev>
2025-08-07 14:41:47 +0200

    (feature) remove feeds-google-shopping dependency
    - Removed ProductFeedController.php that depended on Marshmallow\Feeds\GoogleShopping package
    - Removed routes.php file completely as it contained no active routes
    - Updated ProductServiceProvider to comment out loadRoutesFrom call
    - Eliminates all usage of the feeds-google-shopping package
    
    This removes the dependency completely and cleans up unused route infrastructure.
    
    Resolves GitHub issue #31

--- src/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::group(['namespace' => 'Marshmallow\Product\Http\Controllers'], function () {
-    // Route::post('/', 'ProductController@index');
-    Route::get('/feeds/products/google', 'ProductFeedController@google');
-    // Route::get('/shop/{product:slug}', 'ProductController@show')->name('product.detail');
-});
